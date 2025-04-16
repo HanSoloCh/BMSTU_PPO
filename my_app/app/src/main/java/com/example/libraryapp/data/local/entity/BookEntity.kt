bc5f5e4f62266fe7502722a94e1be96a138e2ff2
Novikov Artem <novikovaa6@student.bmstu.ru>
2025-04-17 00:17:50 +0300

    add some fixs

--- my_app/app/src/main/java/com/example/libraryapp/data/local/entity/BookEntity.kt
+++ my_app/app/src/main/java/com/example/libraryapp/data/local/entity/BookEntity.kt
@@ -4,14 +4,14 @@ import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.ReferenceOption
 
 object BookEntity : UUIDTable("book") {
-    val title = varchar("title", 255)
+    val title = text("title")
     val annotation = text("annotation").nullable()
     val publisherId = reference("publisher_id", PublisherEntity, onDelete = ReferenceOption.CASCADE).nullable()
     val publicationYear = integer("publication_year").nullable()
     val codeISBN = varchar("ISBN", 50).nullable()
     val bbkId = reference("bbk_id", BbkEntity, onDelete = ReferenceOption.RESTRICT)
     val mediaType = varchar("media_type", 100).nullable()
-    val volume = varchar("volume", 100).nullable()
+    val volume = text("volume").nullable()
     val language = varchar("language", 100).nullable()
     val originalLanguage = varchar("original_language", 100).nullable()
     val copies = integer("copies").default(0)
